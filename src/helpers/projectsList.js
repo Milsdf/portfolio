b31b2644217047b064e15b5c34d53baa49c5edb0
Milsdf <milanalatova@gmail.com>
2024-07-27 15:29:31 +0300

    add

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,28 +1,74 @@
-import project01 from "./../img/projects/forecast -smal.png";
+import project01 from "./../img/projects/forecast.png";
 import project01Big from "./../img/projects/forecast.png";
 
-import project02 from "./../img/projects/guitar-smal.png";
+import project02 from "./../img/projects/guitar.png";
 import project02Big from "./../img/projects/guitar.png";
 
-import project03 from "./../img/projects/small-scrol-site.png";
+import project03 from "./../img/projects/scrol-site.png";
 import project03Big from "./../img/projects/scrol-site.png";
 
- import project04 from "./../img/projects/small-boost.png";
- import project04Big from "./../img/projects/bootst.png";
+ import project04 from "./../img/projects/todolist.png";
+ import project04Big from "./../img/projects/todolist.png";
 
-// import project05 from "./../img/projects/05.jpg";
-// import project05Big from "./../img/projects/05-big.jpg";
+import project05 from "./../img/projects/bootst.png";
+import project05Big from "./../img/projects/bootst.png";
 
-// import project06 from "./../img/projects/06.jpg";
-// import project06Big from "./../img/projects/06-big.jpg";
+import project06 from "./../img/projects/quiz.png";
+import project06Big from "./../img/projects/quiz.png";
+
+import project07 from "./../img/projects/clock.png";
+import project07Big from "./../img/projects/clock.png";
+
+import project08 from "./../img/projects/transleter.png";
+import project08Big from "./../img/projects/transleter.png";
+
+import project09 from "./../img/projects/text.png";
+import project09Big from "./../img/projects/text.png";
+
 
 const projects = [
+	{
+		title: 'Bootstrat project',
+		img: project05,
+		imgBig: project05Big,
+		skills: 'Bootstrat',
+		gitHubLink: 'https://milsdf.github.io/bootsrap-project/',
+	},
+	{
+		title: 'TODO LIST',
+		img: project04,
+		imgBig: project04Big,
+		skills: 'HTML CSS JS',
+		gitHubLink: 'https://milsdf.github.io/toDoList/',
+	},
+	{
+		title: 'Translator',
+		img: project08,
+		imgBig: project08Big,
+		skills: 'HTML CSS JS',
+		gitHubLink: 'https://milsdf.github.io/translator/',  
+	},
+	{
+		title: 'Clock',
+		img: project07,
+		imgBig: project07Big,
+		skills: 'HTML CS JS',
+	gitHubLink: 'https://milsdf.github.io/clock/',
+	},
 	{
 		title: 'Weather forecast',
 		skills: 'Api, JS',
 		img: project01,
 		imgBig: project01Big,
 		gitHubLink: ' https://milsdf.github.io/weather-forecast/'
+	},
+	
+	{
+		title: 'Speech to text',
+		skills: 'HTML CSS, JS',
+		img: project09,
+		imgBig: project09Big,
+		gitHubLink: 'https://milsdf.github.io/text/ '
 	},
 	{
 		title: 'Guitar store',
@@ -38,25 +84,15 @@ const projects = [
 		skills: 'css',
 		gitHubLink: 'https://milsdf.github.io/parallax-scrolling/',
 	},
+	
+	
 	{
-		title: 'Bootstrap project',
-		img: project04,
-		imgBig: project04Big,
-		skills: 'Bootstrap',
-		gitHubLink: 'https://milsdf.github.io/bootsrap-project/',
+		title: 'Quiz Game',
+		img: project06,
+		imgBig: project06Big,
+		skills: 'HTML CS JS',
+	gitHubLink: 'https://milsdf.github.io/quiz/',
 	},
-	// {
-	// 	title: 'Landing',
-	// 	img: project05,
-	// 	imgBig: project05Big,
-	// 	skills: 'HTML, SCSS, JS',
-	// },
-	// {
-	// 	title: 'Gaming community',
-	// 	img: project06,
-	// 	imgBig: project06Big,
-	// 	skills: 'React, PHP, MySql',
-	// },
 ];
 
 export {projects}
